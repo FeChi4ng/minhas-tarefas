d77ce7ffa14c26677bde645a5a461e1f3e3790fe
luiz_chiang@hotmail.com <luiz_chiang@hotmail.com>
2026-07-07 09:15:29 -0300

    atualizando o projeto

---
+++ src/components/BotaoAdicionar/styles.ts
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+
+import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
+
+export const Circulo = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 64px;
+  width: 64px;
+  background-color: ${variaveis.verde};
+  color: #fff;
+  position: fixed;
+  bottom: 40px;
+  right: 40px;
+  border-radius: 50%;
+  font-size: 40px;
+  cursor: pointer;
+  text-decoration: none;
+`
